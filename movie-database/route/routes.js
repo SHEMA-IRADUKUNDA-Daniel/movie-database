295aae2478e69be96c97a90c0e69b0b64b536a8f
SHEMA IRADUKUNDA Daniel <danieliradukunda00@gmail.com>
2025-10-03 21:57:08 +0200

    setting up the movie app routes

--- movie-database/route/routes.js
+++ movie-database/route/routes.js
@@ -1,28 +1,34 @@
 import Home from "../src/home";
 import Favorite from "../src/favorite";
+import Layout from "../src/Layout";
 import { createBrowserRouter } from "react-router-dom";
-import App from "../src/App";
 import Login from "../src/login";
+import App from "../src/App";
 
 const routes = createBrowserRouter([
   {
     path: "/",
-    Component: App,
-  },
-  {
-    path: "/home",
-    Component: Home,
-  },
+    Component: Layout,
+    children: [
+      {
+        index: true,
+        Component: App,
+      },
+      {
+        path: "/home",
+        Component: Home,
+      },
 
-  {
-    path: "/favorite",
-    Component: Favorite,
+      {
+        path: "/favorite",
+        Component: Favorite,
+      },
+      {
+        path: "/login",
+        Component: Login,
+      },
+    ],
   },
-  {
-    path: "/login",
-    Component: Login,
-  },
-  // add more routes here
 ]);
 
 export default routes;
